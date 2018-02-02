0b875e5e7544278e027a08010cb4cb1c91b560ae
alex m <uwalex@gmail.com>
2018-02-02 00:17:10 -0500

    Server now supports upload of two files

--- src/www/server.py
+++ src/www/server.py
@@ -24,25 +24,34 @@ def printData():
 	
 @app.route('/uploader', methods = ['POST'])
 def uploader():
+    req_mentor_file = "mentor_file"
+    req_mentee_file = "mentee_file"
+    
     print("uploader() called")
-    print("files:", request.files["file"])
-    if request.method == "POST":
-        #Ensure that the request has the file part
-        if "file" not in request.files:
-            error = "ERROR:", "post request missing file part"
-            return error
+    print("files:", request.files[req_mentor_file], request.files[req_mentee_file])
+   
+    #Ensure that the request has the file part
+    if req_mentor_file not in request.files or req_mentee_file not in request.files:
+        error = "ERROR: post request missing file part"
+        return error
 
-        client_file = request.files["file"]
-        #Check if request was submitted without a file
-        if client_file.filename == "":
-            error = "ERROR:", "User did not attach a file"
-            return error
+    obj_mentor_file = request.files[req_mentor_file]
+    obj_mentee_file = request.files[req_mentee_file]
+    
+    #Check if request was submitted without a file
+    if obj_mentor_file.filename == "" or obj_mentee_file.filename == "":
+        error = "ERROR: User did not attach a file"
+        return error        
 
-        #Actually submitted a file
-        if client_file and allowed_file(client_file.filename):
-            filename  = secure_filename(client_file.filename)
-            client_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "UPLOAD SUCCESS"
+    #Actually submitted a file
+    if obj_mentor_file and allowed_file(obj_mentor_file.filename) and\
+       obj_mentee_file and allowed_file(obj_mentee_file.filename):
+        mentor_filename  = secure_filename(obj_mentor_file.filename)
+        mentee_filename  = secure_filename(obj_mentee_file.filename)
+        
+        obj_mentor_file.save(os.path.join(app.config['UPLOAD_FOLDER'], mentor_filename))
+        obj_mentee_file.save(os.path.join(app.config['UPLOAD_FOLDER'], mentee_filename))
+        return "UPLOAD SUCCESS"
 
 
     return "UPLOAD UNSUCCESSFUL"
