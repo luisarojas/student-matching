557b9ef024073781e47b46f567771d47031dc89c
Luisa Rojas <r.luisaf@gmail.com>
2018-03-09 00:11:16 -0500

    Fixed conversion from categorical data to numerical values.

--- src/scripts/clean_data.py
+++ src/scripts/clean_data.py
@@ -2,6 +2,8 @@ import argparse
 import pandas as pd
 import re
 
+ans_types = {"Yes": 1, "Not Applicable": 0, "No": -1, "Strongly Agree": 2, "Slightly Agree": 1, "Agree": 1, "Neutral": 0, "Slightly Disagree": -1, "Disagree": -1, "Strongly Disagree": -2}
+
 # starting point for the matching process
 # called from the server
 def clean_files(input_file, output_file):
@@ -9,31 +11,30 @@ def clean_files(input_file, output_file):
     # load spreadsheet
     xl = pd.ExcelFile(input_file)
 
-    # load the first sheet into a DataFrame by name: df
+    # load sheet into dataframe
     df = xl.parse(xl.sheet_names[0])
-    df.columns = df.columns.str.strip()
-    df.columns = df.columns.str.replace('\s', '_')  # in case there are multiple white spaces
 
-    # clean up multiple answers for yes/no/not applicable questions
-    for i, header in enumerate(df[:][6:15]):
+    # in the headers, get rid of any extra white space
+    df.columns = df.columns.str.strip()
+
+    # in the headers, replace spaces with underscores
+    df.columns = df.columns.str.replace('\s', '_')
+
+    # clean up answers (e.g. answered more than one option)
+    for i, header in enumerate (df[:][6:]): # all rows, columns 6-15
+
         curr_col = df[header]
 
-        for j, elem in enumerate(curr_col):
-
-            if (re.match("(.+);(Not Applicable)", str(elem))):
-                curr_col = df[header][j] = 'Not Applicable'
-
-            elif (re.match("(Yes);(No)", str(elem))):
-                curr_col = df[header][j] = 'No'
-
+        for j, cell in enumerate(curr_col):
+            if re.match("(.+);(Not Applicable)", str(cell)):
+                curr_col = df[header][j] = "Not Applicable"
+            elif re.match("(Yes);(No)", str(cell)):
+                curr_col = df[header][j] = "No"
             else:
                 pass
 
     # replace all 'Health Science' instances under Faculty for 'Health Sciences'
     df['Faculty'].replace('Health Science', 'Health Sciences', inplace=True)
-    # for i, elem in enumerate(df['Faculty']):
-    #     if elem == 'Health Science':
-    #         df['Faculty'][i] = 'Health Sciences'
 
     # fill-in missing data for columns 6-15 (Yes, No, Not Applicable)
     for i, header in enumerate(df[:][6:15]):
@@ -42,16 +43,20 @@ def clean_files(input_file, output_file):
             if (pd.isnull(elem)):
                 df.set_value(j, header, 'Not Applicable') # assume Not Applicable if empty
 
-    # # fill-in missing data for columns 15-end (Strongly Agree, Agree, ... etc.)
+    # fill-in missing data for columns 15-end (Strongly Agree, Agree, ... etc.)
     for i, header in enumerate(df[:][15:]):
         curr_col = df[header]
         for j, elem in enumerate(curr_col):
-            if (pd.isnull(elem)):
-                df.set_value(j, header, 'Neutral') # assume Neutral if empty
+            if (pd.isnull(elem) or str(elem).lower() == 'Neither Agree or Disagree'.lower()):
+                df.set_value(j, header, 'Neutral')
+            elif (str(elem).lower() == 'Slightly Agree'.lower()):
+                df.set_value(j, header, 'Agree')
+            elif (str(elem).lower() == 'Slightly Disagree'.lower()):
+                df.set_value(j, header, 'Agree')
 
-    # # convert categorical strings to numerical
-    for header in df[df.columns[6:]]:
-        df[header] = df[header].astype('category').cat.codes
+    # convert categorical strings to numerical
+    for i, header in enumerate (df.iloc[:,6:]): # all rows, columns 6-15
+        df[header] = df[header].map(ans_types)
 
     # store dataframe to excel sheet
     writer = pd.ExcelWriter(output_file)
