b797998ab7a70cfff4b0f15a9092d50c77d50a50
Guilherme Damasio <gfdamasio@gmail.com>
2018-03-14 23:17:03 -0400

    Added new restful APIs

--- src/graph_Server/mmm_graph_server/graph_server/views.py
+++ src/graph_Server/mmm_graph_server/graph_server/views.py
@@ -9,127 +9,124 @@ import requests
 
 def check_token(token):
     """Check if the token is valid"""
-    s = requests.Session()
-    s.headers.update({"Content-Type": "application/json"})
-    s.headers.update({"Authorization": token})
-    response = s.get(app.config.get('AUTH_DATABASE_URI'))
-    return response
+    #s = requests.Session()
+    #s.headers.update({"Content-Type": "application/json"})
+    #s.headers.update({"Authorization": token})
+    #response = s.get(app.config.get('AUTH_DATABASE_URI'))
+    #return response
+    return create_success_response("Pass Through")
 
 
-class Students(Resource):
+def create_success_response(result):
+    response_object = {
+        'status': 'success',
+        'data': result
+    }
+    res = jsonify(response_object)
+    res.status_code = 201
+
+    return res
+
+
+def create_message_response(status, message, status_code):
+    response_object = {
+        'status': status,
+        'message': message
+    }
+    res = jsonify(response_object)
+    res.status_code = status_code
+    return res
+
+
+def get_mentors_mentees(is_mentor):
+    try:
+        #token = request.headers['Authorization']
+        #response = check_token(token).json()
+        if True:#response.get('status') == 'success':
+            result = graph.run("MATCH (student:Person {is_mentor:$status}) RETURN student", status=is_mentor).data()
+            return create_success_response(result)
+        #else:
+        #    return create_message_response(response['status'], response['message'], 401)
+    except Exception as e:
+        print(e)
+        return create_message_response('fail', 'Try again', 500)
+
+
+class StudentListAPI(Resource):
     @staticmethod
     def get():
         try:
-            token = request.headers['Authorization']
-            response = check_token(token).json()
-            if response.get('status') == 'success':
+            #token = request.headers['Authorization']
+            #response = check_token(token).json()
+            if True:#response.get('status') == 'success':
                 result = graph.data("MATCH (student:Person) RETURN student")
-                response_object = {
-                    'status': 'success',
-                    'data': result
-                }
-                res = jsonify(response_object)
-                res.status_code = 201
-                return res
-            else:
-                response_object = {
-                    'status': response['status'],
-                    'message': response['message']
-                }
-                res = jsonify(response_object)
-                res.status_code = 401
-                return res
+                return create_success_response(result)
+            #else:
+            #    return create_message_response(response['status'], response['message'], 401)
         except Exception as e:
             print(e)
-            response_object = {
-                'status': 'fail',
-                'message': 'Try again'
-            }
-            res = jsonify(response_object)
-            res.status_code = 500
-            return res
+            return create_message_response('fail', 'Try again', 500)
 
 
-class GetGroup(Resource):
+class StudentAPI(Resource):
+    @staticmethod
+    def get(student_id):
+        try:
+            #token = request.headers['Authorization']
+            #response = check_token(token).json()
+            if True:#response.get('status') == 'success':
+                result = graph.run("MATCH (student:Person {student_id:$id}) RETURN student", id=student_id).data()
+                return create_success_response(result)
+            #else:
+            #    return create_message_response(response['status'], response['message'], 401)
+        except Exception as e:
+            print(e)
+            return create_message_response('fail', 'Try again', 500)
+
+
+class MentorsAPI(Resource):
     @staticmethod
     def get():
-        tutor = {}
-        # args = request.args
-        # faculty = args['faculty']
-        cursor = graph.run(("MATCH (p1:Person {isTutor:true})"
-                            "-[s:SIMILARITY]-"
-                            "(p2:Person {isTutor:false}) "
-                            "WHERE	p1.name <> p2.name "
-                            "WITH     p1, p2, s.euclidian AS e "
-                            "ORDER BY e ASC, p2 DESC "
-                            "RETURN   p1.name AS Person, p2.name AS Neighbor, e AS Similarity"))
+        return get_mentors_mentees(True)
 
-        cand_pair = None
-        assigned_students = []
 
-        while cursor.forward():
-            # print("test")
-            cur_tutor = cursor.current()['Person']
-            cur_student = cursor.current()['Neighbor']
-            similarity = cursor.current()['Similarity']
+class MenteesAPI(Resource):
+    @staticmethod
+    def get():
+        return get_mentors_mentees(False)
 
-            if cur_student in assigned_students:
-                continue
 
-            if cur_tutor not in tutor:
-                tutor[cur_tutor] = []
-
-            if cand_pair is not None:
-                if cand_pair[1] == cur_student and cand_pair[2] == similarity:
-                    if len(tutor[cand_pair[0]]) > len(tutor[cur_tutor]):
-                        # print("Changing " + cand_pair[0] + " to " + cur_tutor)
-                        # print(str(len(tutor[cand_pair[0]])) + " > " + str(len(tutor[cur_tutor])))
-                        cand_pair = (cur_tutor, cand_pair[1], cand_pair[2])
-                else:
-                    tutor[cand_pair[0]].append(cand_pair[1])
-                    assigned_students.append(cand_pair[1])
-                    cand_pair = (cur_tutor, cur_student, similarity)
-                    continue
-            else:
-                if len(tutor[cur_tutor]) <= 5:
-                    print("Assigning to " + cur_tutor)
-                    cand_pair = (cur_tutor, cur_student, similarity)
-                continue
-
-            if len(tutor[cur_tutor]) <= 5:
-                print("Assigning " + cur_student + " to " + cur_tutor + " " + str(len(tutor[cur_tutor])))
-                if cur_student not in assigned_students:
-                    # Save as possible solution
-                    candidate_pair = (cur_tutor, cur_student, similarity)
-
-        # print(tutor)
-        return tutor
+class GroupAPI(Resource):
+    @staticmethod
+    def get(student_id):
+        try:
+            stmt = """
+MATCH(p:Person {student_id:$id})-[:PART_OF_GROUP]-(mentor:Person {is_mentor:true})
+ WITH mentor
+ MATCH(student:Person)-[:PART_OF_GROUP]-(mentor) RETURN student
+"""
+            result = graph.run(stmt, id=student_id).data()
+            return create_success_response(result)
+        except Exception as e:
+            print(e)
+            return create_message_response('fail', 'Try again', 500)
 
 
 class GroupInsertion(Resource):
     @staticmethod
     def put():
         try:
-            #print(request.get_json())
             create_data(request.get_json())
-            response_object = {
-                'status': 'success'
-            }
-            res = jsonify(response_object)
-            res.status_code = 201
-            return res
+            return create_message_response('success', 'Data Inserted', 201)
         except Exception as e:
             print(e)
-            response_object = {
-                'status': 'fail',
-                'message': 'Try again'
-            }
-            res = jsonify(response_object)
-            res.status_code = 500
-            return res
+            return create_message_response('fail', 'Try again', 500)
 
 
 # Setting the routes
-api.add_resource(Students, '/students')
-api.add_resource(GetGroup, '/groups')
+api.add_resource(StudentListAPI, '/students')
+api.add_resource(StudentAPI, '/students/<int:student_id>')
+api.add_resource(MentorsAPI, '/students/mentors/')
+api.add_resource(MenteesAPI, '/students/mentees/')
+api.add_resource(GroupAPI, '/groups/<int:student_id>')
 api.add_resource(GroupInsertion, '/groupInsertion')
